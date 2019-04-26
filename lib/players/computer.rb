5aa4155179388641b6c271ba84c5679850f6fa18
Alexander Baker <alex.baker685@gmail.com>
2019-04-26 21:54:29 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,9 +3,9 @@ class Players
   class Computer < Player
 
     def move(board)
-      puts 'Please enter 1-9:'
-      computer_input = ["1","2","3","4","5","6","7","8","9"].sample
-      computer_input
+      # puts 'Please enter 1-9:'
+      # computer_input = ["1","2","3","4","5","6","7","8","9"].sample
+      # computer_input
     end
 
   end
