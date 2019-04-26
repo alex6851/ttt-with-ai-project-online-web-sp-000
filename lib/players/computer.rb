2b2d9d3b6502cf791e3fb9a09471a0c630dae3fe
Alexander Baker <alex.baker685@gmail.com>
2019-04-26 21:57:06 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -10,9 +10,9 @@ class Players
       if !board.taken(5)
         move = "5"
       elsif board.turn_count == 1
-        move = "1" 
+        move = "1"
       elsif board.turn_count == 2
-      move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s 
+      move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
     end
 
   end
