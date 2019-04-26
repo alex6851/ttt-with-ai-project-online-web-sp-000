ded45b97a938a9831ad0b4010f6b5480db654ade
Alexander Baker <alex.baker685@gmail.com>
2019-04-26 21:54:49 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,6 +6,8 @@ class Players
       # puts 'Please enter 1-9:'
       # computer_input = ["1","2","3","4","5","6","7","8","9"].sample
       # computer_input
+      if !board.taken(5)
+        move = "5"
     end
 
   end
